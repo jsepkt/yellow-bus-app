2fe028bfc9b81a7f73f3df1d67babc9737be6a8c
jsepkt <120427427+jsepkt@users.noreply.github.com>
2025-05-05 18:00:49 -0500

    Add collectstatic to build command 2

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    route_number = models.CharField(max_length=20)
+    
+    profile_picture = models.ImageField(
+        upload_to='students/',  # will be saved under /media/students/
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
